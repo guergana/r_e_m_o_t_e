5069ed12b62896149ac2720d39fbdab2b0190376
Guergana <pestanias@yahoo.com>
2019-12-20 15:36:49 +0100

    removed build folder from gitignore

---
+++ client/build/precache-manifest.b26b0583f712819f206f74a34c8f4820.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7c72fac2268fd531169bf77c34e51bb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "30d9cb5da6f137cf3187",
+    "url": "/static/css/main.394ca612.chunk.css"
+  },
+  {
+    "revision": "e70c034112880604ed86",
+    "url": "/static/js/2.5496fbed.chunk.js"
+  },
+  {
+    "revision": "138a6d51240ab070e8d1b20c54be6394",
+    "url": "/static/js/2.5496fbed.chunk.js.LICENSE"
+  },
+  {
+    "revision": "30d9cb5da6f137cf3187",
+    "url": "/static/js/main.8181feb6.chunk.js"
+  },
+  {
+    "revision": "2bcb8084869f23f72706",
+    "url": "/static/js/runtime-main.88d4f3f8.js"
+  },
+  {
+    "revision": "89f9e47f761538040eb5be44c85f6328",
+    "url": "/static/media/liberationmono-bold-webfont.89f9e47f.woff2"
+  },
+  {
+    "revision": "a3effeff5dc9a5892d02daee33372d7d",
+    "url": "/static/media/liberationmono-bold-webfont.a3effeff.woff"
+  },
+  {
+    "revision": "22015ce054cccb8811292411b0605355",
+    "url": "/static/media/liberationmono-regular-webfont.22015ce0.woff"
+  },
+  {
+    "revision": "8b7cacc11487587b395b469818ab99c2",
+    "url": "/static/media/liberationmono-regular-webfont.8b7cacc1.woff2"
+  }
+]);
